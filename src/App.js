7fec32a8927b89331b1b2e07c2d9514d5689defa
hardi shah <hardishahhardi@gmail.com>
2021-01-06 22:56:25 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Layout/Navbar';
+import Home from './components/Categories/Pages/Home';
 
 function App() {
   return (
     <div className="App">
-   <Navbar/>
+   {/* <Navbar/> */}
+   <Home/>
     </div>
   );
 }
